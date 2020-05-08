bbea1d3d018136ba479d65f283665f380f2b0e4d
Nathan Pickard <nathan.pickard87@gmail.com>
2020-05-07 17:23:12 -0700

    Project scaffolding

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy('./_site/images');
+  
+  return {
+    dir: {
+      input: '_site',
+      output: 'dist'
+    }
+  }
+}
